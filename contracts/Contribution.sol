adb32915a29997cc26f372295402c011dac25615
Kieran Prasch <kieranprasch@gmail.com>
2023-06-26 22:52:10 +0200

    Introduces gamified countdown dynamics and parameterization of contribution rules.

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -1,41 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 contract Contribution {
     address payable public immutable owner;
+    address payable public immutable beneficiary;
     uint public totalContributed;
-    bool public isReleased;
-    uint256 public dropTime;
+    bool public materialReleased;
+    uint256 public countdownPeriod;
+    uint256 public threshold;
+
+    mapping(address => uint256) public contributors;
+    mapping(address => bool) public contributorsBeforeReveal;
 
     event Contribution(address indexed contributor, uint256 amount);
     event FundsReleased();
     event Sweep();
 
-    constructor(uint256 dropTime) {
+    constructor(
+        uint256 countdownPeriod,
+        uint256 _threshold,
+        address payable _beneficiary
+    ) {
         owner = payable(msg.sender);
-        dropTime = dropTime;
-    }
-
-    function release() external {
-        require(msg.sender == owner, "Only the contract owner can release funds");
-        require(block.timestamp >= dropTime, "Funds cannot be released before the album drop time");
-        isReleased = true;
-        emit FundsReleased();
+        countdownPeriod = countdownPeriod;
+        threshold = _threshold;
+        beneficiary = _beneficiary;
     }
 
     function contribute() external payable {
-        require(msg.value >= 0.05 ether, "Contribution must be at least 0.05 ETH");
         totalContributed += msg.value;
-        if (totalContributed >= 10 ether) {
-            isReleased = true;
+        contributors[msg.sender] += msg.value;
+
+        // Flag contributors who have contributed before the threshold is met
+        if (totalContributed - msg.value < threshold) {
+            contributorsBeforeReveal[msg.sender] = true;
         }
+
+        // This is "contribution-revealer logic"
+        if (totalContributed >= threshold) {
+            // Mark the material as released
+            materialReleased = true;
+        }
+
+        // This is domain-specific logic....?
+        if (materialReleased) {
+            // Reset the countdown period
+            endTime = block.timestamp + countDownPeriod;
+        }
+
+        emit Contribution(msg.sender, msg.value);
+    }
+
+    receive() external payable {
         emit Contribution(msg.sender, msg.value);
     }
 
     function withdraw() external {
-        require(msg.sender == owner, "Only the contract owner can withdraw funds");
-        require(isReleased, "Funds have not yet been released");
-        owner.transfer(address(this).balance);
+        require(msg.sender == beneficiary, "Only the beneficiary can withdraw funds");
+        require(materialReleased, "Funds have not yet been released");
+        beneficiary.transfer(address(this).balance);
         emit Sweep();
+    }
+
+    function contributedAfterThreshold(address _contributor) external view returns (bool) {
+        return contributors[_contributor] > 0 && !contributorsBeforeReveal[_contributor];
     }
 }
