44f07d4174c5907d1eb43f68d1f9f6d5accdf17b
Richard Partington III <>
2023-07-21 17:32:20 +0200

    Forgot to add the javascript for the .tooltop feature.

---
+++ webapp/help.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    tippy('#min-preset', {
+        content: ".1 ETH is the minimum",
+    });
+
+    tippy('#ten-preset', {
+        content: "The top 10 bidders will receive NFT artifacts bound to unique painting by a Nashville artist",
+    });
+
+    tippy('#leader-preset', {
+        content: "The top bidder will receive the #1 artifact bound to unique painting by a Nashville artist",
+    });
+});
+
+
