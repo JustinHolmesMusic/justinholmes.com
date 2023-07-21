08e1d381e5fc43ae500e52814cbd55fd18788e55
Richard Partington III <>
2023-07-21 18:18:34 +0200

    Changed .tooltip so that it appears below the buttons, also removed a myster div and reformatted.

--- webapp/help.js
+++ webapp/help.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     tippy('#min-preset', {
         content: ".1 ETH is the minimum",
+        placement: "bottom"
     });
 
     tippy('#ten-preset', {
         content: "The top 10 bidders will receive NFT artifacts bound to unique painting by a Nashville artist",
+        placement: "bottom"
     });
 
     tippy('#leader-preset', {
         content: "The top bidder will receive the #1 artifact bound to unique painting by a Nashville artist",
+        placement: "bottom"
     });
 });
 
