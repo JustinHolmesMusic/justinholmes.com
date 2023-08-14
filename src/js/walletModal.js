3c907e572a3c9b84884a9e8604cc7d06ca503f4b
jMyles <justin@justinholmes.com>
2023-08-14 18:12:50 -0500

    Making devserver work so we don't have to have watch in one tab and a localserver in another.

--- src/js/walletModal.js
+++ src/js/walletModal.js
@@ -1,10 +1,10 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 import { configureChains, createConfig } from '@wagmi/core'
-import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
+import { mainnet, goerli } from '@wagmi/core/chains'
 
 // Equivalent to importing from @wagmi/core/providers
-const chains = [arbitrum, mainnet, polygon]
+const chains = [mainnet, goerli]
 const projectId = '3e6e7e58a5918c44fa42816d90b735a6'
 
 const {publicClient} = configureChains(chains, [w3mProvider({projectId})])
