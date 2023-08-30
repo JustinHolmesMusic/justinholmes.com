f32863f527ac4f98be9f0d2fce799939285c3e93
Jakub Hejhal <kubic71@gmail.com>
2023-08-30 22:44:37 +0200

    update contract ABI

--- src/js/contributionABI.js
+++ src/js/contributionABI.js
@@ -3,18 +3,37 @@ export const contractABI = [{
         "internalType": "uint256",
         "name": "_countdownPeriod",
         "type": "uint256"
-    }, {"internalType": "uint256", "name": "_threshold", "type": "uint256"}, {
+    }, {
+        "internalType": "uint256",
+        "name": "_threshold",
+        "type": "uint256"
+    }, {
         "internalType": "uint256",
         "name": "_minContribution",
         "type": "uint256"
-    }, {"internalType": "address payable", "name": "_beneficiary", "type": "address"}, {
+    }, {
+        "internalType": "uint256",
+        "name": "_initialWindow",
+        "type": "uint256"
+    }, {
+        "internalType": "address payable",
+        "name": "_beneficiary",
+        "type": "address"
+    }, {
         "internalType": "bool",
         "name": "_testnet",
         "type": "bool"
-    }], "stateMutability": "nonpayable", "type": "constructor"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
 }, {
     "anonymous": false,
-    "inputs": [{"indexed": false, "internalType": "uint256", "name": "deadline", "type": "uint256"}],
+    "inputs": [{
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "deadline",
+        "type": "uint256"
+    }],
     "name": "ClockReset",
     "type": "event"
 }, {
@@ -24,12 +43,22 @@ export const contractABI = [{
         "internalType": "address",
         "name": "contributor",
         "type": "address"
-    }, {"indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256"}],
+    }, {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+    }],
     "name": "Contribute",
     "type": "event"
 }, {
     "anonymous": false,
-    "inputs": [{"indexed": true, "internalType": "address", "name": "lastContributor", "type": "address"}],
+    "inputs": [{
+        "indexed": true,
+        "internalType": "address",
+        "name": "lastContributor",
+        "type": "address"
+    }],
     "name": "Decryptable",
     "type": "event"
 }, {
@@ -39,77 +68,152 @@ export const contractABI = [{
         "internalType": "address",
         "name": "beneficiary",
         "type": "address"
-    }, {"indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256"}],
+    }, {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+    }],
     "name": "Withdraw",
     "type": "event"
 }, {
     "inputs": [],
     "name": "artifactContract",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "outputs": [{
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "beneficiary",
-    "outputs": [{"internalType": "address payable", "name": "", "type": "address"}],
+    "outputs": [{
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "bytes32", "name": "_hash", "type": "bytes32"}, {
+    "inputs": [{
+        "internalType": "bytes32",
+        "name": "_hash",
+        "type": "bytes32"
+    }, {
         "internalType": "bytes",
         "name": "_ciphertext",
         "type": "bytes"
-    }], "name": "commitSecret", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-}, {"inputs": [], "name": "contribute", "outputs": [], "stateMutability": "payable", "type": "function"}, {
+    }],
+    "name": "commitSecret",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "contribute",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+}, {
     "inputs": [],
     "name": "contributeAndCombine",
     "outputs": [],
     "stateMutability": "payable",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "name": "contributionAmounts",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "name": "contributionDatetimes",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "name": "contributionIsCombined",
-    "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "name": "contributorsForEachAContribution",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "contributorsForEachContribution",
+    "outputs": [{
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "countdownPeriod",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "deadline",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "getAllContributions",
-    "outputs": [{"internalType": "address[]", "name": "", "type": "address[]"}, {
+    "outputs": [{
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+    }, {
         "internalType": "uint256[]",
         "name": "",
         "type": "uint256[]"
-    }, {"internalType": "bool[]", "name": "", "type": "bool[]"}, {
+    }, {
+        "internalType": "bool[]",
+        "name": "",
+        "type": "bool[]"
+    }, {
         "internalType": "uint256[]",
         "name": "",
         "type": "uint256[]"
@@ -118,44 +222,82 @@ export const contractABI = [{
     "type": "function"
 }, {
     "inputs": [],
+    "name": "initialWindow",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
     "name": "isKeySet",
-    "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "keyCiphertext",
-    "outputs": [{"internalType": "bytes", "name": "", "type": "bytes"}],
+    "outputs": [{
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "keyPlaintext",
-    "outputs": [{"internalType": "bytes", "name": "", "type": "bytes"}],
+    "outputs": [{
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "keyPlaintextHash",
-    "outputs": [{"internalType": "bytes32", "name": "", "type": "bytes32"}],
+    "outputs": [{
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "materialReleaseConditionMet",
-    "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "minContribution",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "owner",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "outputs": [{
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
@@ -165,25 +307,41 @@ export const contractABI = [{
     "stateMutability": "nonpayable",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "bytes", "name": "secret", "type": "bytes"}],
+    "inputs": [{
+        "internalType": "bytes",
+        "name": "secret",
+        "type": "bytes"
+    }],
     "name": "revealSecret",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "address", "name": "_artifactContract", "type": "address"}],
+    "inputs": [{
+        "internalType": "address",
+        "name": "_artifactContract",
+        "type": "address"
+    }],
     "name": "setArtifactContract",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "bool", "name": "status", "type": "bool"}],
+    "inputs": [{
+        "internalType": "bool",
+        "name": "status",
+        "type": "bool"
+    }],
     "name": "setMaterialReleaseConditionMet",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "uint256", "name": "_threshold", "type": "uint256"}],
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "_threshold",
+        "type": "uint256"
+    }],
     "name": "setThreshold",
     "outputs": [],
     "stateMutability": "nonpayable",
@@ -191,19 +349,35 @@ export const contractABI = [{
 }, {
     "inputs": [],
     "name": "testnet",
-    "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
     "inputs": [],
     "name": "threshold",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
-    "inputs": [{"internalType": "address", "name": "contributor", "type": "address"}],
+    "inputs": [{
+        "internalType": "address",
+        "name": "contributor",
+        "type": "address"
+    }],
     "name": "totalContributedByAddress",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
     "stateMutability": "view",
     "type": "function"
 }, {
@@ -212,6 +386,10 @@ export const contractABI = [{
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
-}, {"stateMutability": "payable", "type": "receive"}]
+}, {
+    "stateMutability": "payable",
+    "type": "receive"
+}]
+
 
 export default contractABI;
