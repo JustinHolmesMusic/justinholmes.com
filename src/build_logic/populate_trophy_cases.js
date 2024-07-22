c2b45e60dcbd52a949f277898e5d4ab6399b176e
Jakub Hejhal <kubic71@gmail.com>
2024-07-22 23:34:04 +0200

    chain data now contrains shows with setStone attributes merged from local yaml files and on-chain data # almost midnight in Copenhagen

--- src/build_logic/populate_trophy_cases.js
+++ src/build_logic/populate_trophy_cases.js
@@ -3,6 +3,14 @@ import {mainnet, optimism, optimismSepolia} from '@wagmi/core/chains';
 import {brABI as abi} from "../abi/blueRailroadABI.js";
 import {setStoneABI} from "../abi/setStoneABI.js";
 import {shows} from "./show_and_set_data.js";
+import { JsonRpcVersionUnsupportedError } from 'viem';
+
+function stringify(obj) {
+    // Custom replacer function to handle BigInt
+    return JSON.stringify(obj, (key, value) =>
+        typeof value === 'bigint' ? value.toString() : value, 2);
+}
+
 
 export const config = createConfig({
     chains: [mainnet, optimism, optimismSepolia],
@@ -16,25 +24,56 @@ export const config = createConfig({
 const blueRailroadAddress = "0xCe09A2d0d0BDE635722D8EF31901b430E651dB52";
 const setStoneContractAddress = "0x111c9369B385caF848E7F453799ACaC4d4Ced589";
 
-let showSetStoneData = {}
 
 // Iterate through show IDs and parse the data.
-for (let [song_title, song_data] of Object.entries(shows)) {
+const showsJson = JSON.stringify(shows, null, 2);
+// console.log("shows", showsJson);
+
+for (let [show_id, show] of Object.entries(shows)) {
+
+    // console.log("show_id", show_id);
+    // console.log("show", JSON.stringify(show, null, 2));
 
     // Split ID by "-" into artist_id and blockheight
-    const showID = liveShowIDs[i];
-    const [artist_id, blockheight] = showID.split('-');
+    const [artist_id, blockheight] = show_id.split('-');
 
     // Read the contract using the getShowData function
     const showData = await readContract(config, {
-        abi: setStoneABI,
-        address: setStoneContractAddress,
-        functionName: 'getShowData',
-        chainId: optimismSepolia.id,
-        args: [artist_id, blockheight],
+     abi: setStoneABI,
+     address: setStoneContractAddress,
+     functionName: 'getShowData',
+     chainId: optimismSepolia.id,
+     args: [artist_id, blockheight],
     });
-    showSetStoneData[showID] = showData;
+
+    // (bytes32 showBytes1, uint16 stonesPossible1, uint8 numberOfSets1, uint256 stonePrice1, bytes32[] memory rabbitHashes1)
+    // This is the return type of the solidity getShowData function
+    // unpack the showData
+    const unpackedShowData = {
+        showBytes: showData[0],
+        stonesPossible: showData[1],
+        numberOfSets: showData[2],
+        stonePrice: showData[3],
+        rabbitHashes: showData[4],
+    };
+    // console.log("Unpacked Show Data:", unpackedShowData);
+
+    // console.log("showData:");
+    // console.log(showData);
+
+    show["rabbit_hashes"] = unpackedShowData.rabbitHashes;
+    show["stone_price"] = unpackedShowData.stonePrice;
+
+    // check that the number of sets on chain is the same as the number of sets in the yaml, raise an error if not
+    if (unpackedShowData.numberOfSets !== Object.keys(show.sets).length) {
+        throw new Error(`Number of sets on chain (${unpackedShowData.numberOfSets}) does not match the number of sets in the yaml (${show.sets.length}) for show ID ${show_id}`);
+    }
+
+    // console.log("show", stringify(show));
+    // showSetStoneData[showID] = showData;
 }
+
+// console.log(stringify(shows));
 
 
 ///////////BACK TO TONY
@@ -98,6 +137,5 @@ export const chainData = {
     mainnetBlockNumber: mainnetBlockNumber,
     optimismBlockNumber: optimismBlockNumber,
     optimismSepoliaBlockNumber: optimismSepoliaBlockNumber,
-    liveShowIDs: liveShowIDs,
-    showSetStoneData: showSetStoneData,
+    showSetStoneData: shows,
 }
