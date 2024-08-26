b22b7b92532dae4632d592be34e3c642bee1817e
jMyles <justin@justinholmes.com>
2024-08-26 14:09:08 +0200

    Displaying blockheights with commas for easier reading.

--- src/build_logic/utils/template_helpers.js
+++ src/build_logic/utils/template_helpers.js
@@ -10,35 +10,35 @@ let _helpers_are_registered = false;
 
 export function registerHelpers() {
 
-  if (_helpers_are_registered) {
-    console.warn('Helpers are already registered');
-    return;
-  }
+    if (_helpers_are_registered) {
+        console.warn('Helpers are already registered');
+        return;
+    }
 
-  // Register the Handlebars helper
-  Handlebars.registerHelper('blockToDate', (blockHeight, timeZone) => {
-    const blockDifference = blockHeight - REFERENCE_BLOCK;
-    const timeDifference = blockDifference * AVERAGE_BLOCK_TIME;
-    const estimatedTimestamp = REFERENCE_TIMESTAMP + timeDifference;
+    // Blockheight - datetime
+    Handlebars.registerHelper('blockToDate', (blockHeight, timeZone) => {
+        const blockDifference = blockHeight - REFERENCE_BLOCK;
+        const timeDifference = blockDifference * AVERAGE_BLOCK_TIME;
+        const estimatedTimestamp = REFERENCE_TIMESTAMP + timeDifference;
 
-    // Create a Luxon DateTime object
-    const date = DateTime.fromSeconds(estimatedTimestamp, { zone: timeZone });
+        // Create a Luxon DateTime object
+        const date = DateTime.fromSeconds(estimatedTimestamp, {zone: timeZone});
 
-    // Format the date as desired
-    const formatted_date = date.toFormat('MMMM dd, yyyy, h:mm a'); // e.g., "January 01, 2023, 2:30:45 PM EST";
-    return formatted_date;
-  });
-  _helpers_are_registered = true;
+        // Format the date as desired
+        const formatted_date = date.toFormat('MMMM dd, yyyy, h:mm a'); // e.g., "January 01, 2023, 2:30:45 PM EST";
+        return formatted_date;
+    });
+
+    // RnumberWithCommas
+    Handlebars.registerHelper('numberWithCommas', function (number) {
+        // Ensure the input is a number
+        if (typeof number !== 'number') {
+            number = parseFloat(number);
+        }
+
+        // Return the number formatted with commas
+        return number.toLocaleString();
+    });
+
+    _helpers_are_registered = true;
 }
-//
-// // Register the Handlebars helper
-// Handlebars.registerHelper('blockToDate', (blockHeight, timeZone = 'UTC') => {
-//   const blockDifference = blockHeight - REFERENCE_BLOCK;
-//   const estimatedTimestamp = REFERENCE_TIMESTAMP + (blockDifference * AVERAGE_BLOCK_TIME);
-//
-//   // Create a Luxon DateTime object
-//   const date = DateTime.fromSeconds(estimatedTimestamp, { zone: timeZone });
-//
-//   // Format the date as desired
-//   return date.toFormat('MMMM dd, yyyy, h:mm:ss a ZZZZ'); // e.g., "January 01, 2023, 2:30:45 PM EST"
-// });
