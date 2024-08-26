782cab41c964d04f30377cd52783bf08d0cb60c5
jMyles <justin@justinholmes.com>
2024-08-26 20:38:23 +0200

    Moving the whole kaboodle to shows display.

--- src/build_logic/primary_builder.js
+++ src/build_logic/primary_builder.js
@@ -229,8 +229,7 @@ Object.entries(chainData.showsWithChainData).forEach(([show_id, show]) => {
 
     // TODO: the URL should look like
     // https://justinholmes.com/cryptograss/bazaar/setstone/<artist_id>-<blockheight>.html?secret_rabbit=%3Ccode%3E
-    const outputFilePath = path.join(outputBaseDir, `cryptograss/bazaar/setstones/${show_id}.html`);
-
+    const outputFilePath = path.join(outputBaseDir, `shows/${show_id}.html`);
 
     const hbsTemplate = path.join(templateDir, 'pages/cryptograss/bazaar/strike-set-stones.hbs');
 
